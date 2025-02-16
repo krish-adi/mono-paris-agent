e4189e51598775a8940a32a67a2ab362a45841fc
Dylan Merigaud <merigaudconsulting@gmail.com>
2025-02-16 19:37:36 +0100

    button

--- frontend/app/report/[reportId]/page.tsx
+++ frontend/app/report/[reportId]/page.tsx
@@ -3,6 +3,7 @@ import { Dashboard } from "../../dashboard";
 import Link from "next/link";
 import { createClient } from "@/utils/supabase/server";
 import { Tables } from "@/lib/database.types";
+import { ArrowLeft } from "lucide-react";
 
 export default async function Report({
   params,
@@ -14,8 +15,11 @@ export default async function Report({
   return (
     <main className="flex-1 flex flex-col gap-6 px-8 max-w-3xl w-svw">
       <Dashboard reportId={reportId} />
-      <Button className="my-4" asChild variant="outline">
-        <Link href="/">Find out for other roles</Link>
+      <Button className="my-4" asChild variant="outline" size="lg">
+        <Link href="/">
+          <ArrowLeft className="mr-2 h-4 w-4" />
+          Find out for other roles
+        </Link>
       </Button>
     </main>
   );
