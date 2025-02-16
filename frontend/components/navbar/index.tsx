008ffde2e6b15c06f5a91749b0ea1e29acac0aa4
Adithya Krishnan <krishsandeep@gmail.com>
2025-02-16 10:07:24 +0100

    setup a navbar

--- frontend/components/navbar/index.tsx
+++ frontend/components/navbar/index.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import { ThemeSwitcher } from "./theme-switcher";
+import Link from "next/link";
 
 export default function index() {
     return (
-        <nav className="w-full flex justify-center border-b border-b-foreground/10 h-8">
-            <ThemeSwitcher />
-            <p>IRREPLACABLE</p>
+        <nav className="w-full flex justify-between items-center border-b border-b-foreground/10 py-2 px-4">
+            <div className="text-md font-bold">IRREPLACABLE</div>
+            <div className="flex items-center gap-2">
+                <Link href="/">Home</Link>
+                <Link href="/sign-in">Sign In</Link>
+                <Link href="/sign-up">Sign Up</Link>
+                <Link href="/u">Console</Link>
+                <ThemeSwitcher />
+            </div>
         </nav>
     );
 }
